0b060bea172f70a0010d9a5dad08af8586562f55
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2023-11-04 19:52:16 -1200

    fix,descarga mp4 en memoria

--- services/downloadMp4.js
+++ services/downloadMp4.js
@@ -1,8 +1,40 @@
-const { Video } = require("../database/models/video")
-const { uploadVideoToCloudinaryFromStream } = require("../utils/claurinary")
+const MemoryFS = require('memory-fs');
+const memoryFs = new MemoryFS();
 const { isUrlLive, isVideoLive, downloadMedia } = require("../utils/helpers")
 
+
+const pathMemori = "/mp4";
+const fileTimestamps = {}
+function cleanMemoryFs() {
+
+    const filesInMemoryFs = memoryFs.readdirSync(pathMemori);
+    console.log('Archivos en memoria:');
+    if (filesInMemoryFs.length === 0) {
+        console.log('La memoria está vacía. No se realizó ninguna limpieza.');
+        return;
+    }
+
+    filesInMemoryFs.forEach((fileName) => {
+        memoryFs.unlinkSync(`${pathMemori}/${fileName}`);
+        delete fileTimestamps[fileName];
+        console.log(`Eliminado de memory-fs: ${fileName}`);
+    });
+}
+
+memoryFs.mkdirSync(pathMemori, { recursive: true })
+
+function storeFileInMemoryFs(fileName, fileData) {
+
+    const filePath = `${pathMemori}/${fileName}`
+    memoryFs.writeFileSync(filePath, fileData)
+    fileTimestamps[fileName] = new Date().getTime() // Registrar la hora de almacenamiento
+    return filePath
+}
+
 const downloadMp4Service = async (url, mediaType) => {
+    const timestamp = new Date().getTime()
+
+
 
     if (isUrlLive(url)) {
         throw new Error("¡Ups!, no se pueden descargar videos en vivo")
@@ -11,7 +43,9 @@ const downloadMp4Service = async (url, mediaType) => {
     if (chechLive) {
         throw new Error("¡Ups!, no se pueden descargar videos en vivo")
     }
+    cleanMemoryFs()
 
+    const fileName = `${timestamp}.${mediaType === 'video' ? 'mp4' : 'mp3'}`
     const audioStream = await downloadMedia(url, mediaType)
 
     const audioBuffer = []
@@ -24,17 +58,12 @@ const downloadMp4Service = async (url, mediaType) => {
 
         audioStream.on('end', async () => {
             try {
-                const audioData = Buffer.concat(audioBuffer);
-             /*    const cloudinaryUrl = await uploadVideoToCloudinaryFromStream(audioData);
- */           const video = new Video({
-                    title: 'Mi video',
-                    data: audioData,
-                    contentType: 'video/mp4',
-                });
-                video.save()
-                const result = await Video.findById(video._id)
+                const audioData = Buffer.concat(audioBuffer)
+                const store = storeFileInMemoryFs(fileName, audioData)
+                const fileData = memoryFs.readFileSync(store)
+                resolve(fileData)
+                console.log(`Creado memory-fs: ${store}`)
 
-                resolve(result.data);
             } catch (error) {
                 reject(error);
             }
